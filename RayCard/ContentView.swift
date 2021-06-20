f6a17a7223a0b12347b16d8ab3f5a824b722b8aa
Ganthology <gbk1386@gmail.com>
2021-06-20 13:48:31 +0800

    Initial Commit

---
+++ RayCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RayCard
+//
+//  Created by Boon Kit Gan on 20/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
